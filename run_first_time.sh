f725af6d48503fa8a39813bf71878abc6b9c3a4f
Cid Lopes <alannettto@gmail.com>
2024-04-24 17:12:52 +0300

    Update run_first_time script

--- run_first_time.sh
+++ run_first_time.sh
@@ -2,13 +2,14 @@
 
 set -e
 
-docker-compose down -v --remove-orphans
+bin/removeall
 
 rm -rf src
 
-bin/download 2.4.4
-bin/setup magento2.dev.nos.to:8443
+bin/download 2.4.7 community
+bin/setup magento245.dev.nos.to:8443
 
+bin/magento module:disable Magento_AdminAdobeImsTwoFactorAuth
 bin/magento module:disable Magento_TwoFactorAuth
 # bin/magento module:disable Magento_Csp --clear-static-content
 
@@ -18,9 +19,9 @@ bin/composer config --unset repositories.0
 # bin/composer require --no-update nosto/module-nostotagging:"@stable" 
 # bin/composer update --no-dev
 
-bin/composer require --no-update nosto/module-nostotagging:dev-feature/2.4.4-compatibility
-# composer require --no-update nosto/module-nostotagging-cmp:dev-develop
-# composer require --no-update nosto/module-nosto-msi:@stable
+bin/composer require --no-update nosto/module-nostotagging:"@stable"
+bin/composer require --no-update nosto/module-nostotagging-cmp:"@stable"
+bin/composer require --no-update nosto/module-nosto-msi:"@stable"
 
 bin/composer config repositories.0 '{"type": "composer", "url": "https://repo.magento.com", "exclude": ["nosto/*"]}'
 bin/composer update --no-dev
@@ -39,27 +40,35 @@ bin/magento config:set nosto/flags/low_stock_indication 1
 bin/magento config:set nosto/flags/send_customer_data 1
 bin/magento config:set nosto/flags/indexer_memory 70
 bin/magento config:set nosto/flags/tag_date_published 1
-# bin/magento config:set nosto_cmp/flags/category_sorting 1
-# bin/magento config:set nosto_cmp/flags/map_all_categories 1
-# bin/magento cache:flush
+bin/magento config:set nosto_cmp/flags/category_sorting 1
+bin/magento config:set nosto_cmp/flags/map_all_categories 1
+bin/magento cache:flush
 
 
-bin/magento module:enable --clear-static-content Nosto_Tagging
+bin/magento module:enable --clear-static-content Nosto_Tagging Nosto_Cmp Nosto_Msi
 bin/magento setup:upgrade
 bin/magento setup:di:compile
 bin/magento setup:static-content:deploy -f
 bin/magento cache:clean
 
-cp env/nosto.env src/vendor/nosto/php-sdk/src/.env
-bin/copytocontainer vendor/nosto/php-sdk/src/
+bin/copyfromcontainer vendor/nosto
+# cp env/nosto.env src/vendor/nosto/php-sdk/src/.env
+# bin/copytocontainer vendor/nosto/php-sdk/src/
 
-# Uncoment if you're doing local development, this sets up the git repo:
-# git clone git@github.com:Nosto/nosto-magento2.git src/vendor/nosto/module-nostotagging
-# git clone git@github.com:Nosto/nosto-magento2-cmp.git src/vendor/nosto/module-nostotagging-cmp
-# bin/copytocontainer vendor/nosto
-# bin/magento module:enable --clear-static-content Nosto_Tagging Nosto_Cmp
-# bin/magento setup:upgrade
-# bin/magento setup:di:compile
-# bin/magento setup:static-content:deploy -f
-# bin/magento cache:clean
-# echo "You should be set for local development. Remember to add magento src to PHPStorm for code completion"
+# If you're doing local development, this sets up the git repositories:
+rm -rf src/vendor/nosto/module-nostotagging
+rm -rf src/vendor/nosto/module-nostotagging-cmp
+rm -rf src/vendor/nosto/module-nosto-msi
+rm -rf src/vendor/nosto/php-sdk
+git clone git@github.com:Nosto/nosto-magento2.git src/vendor/nosto/module-nostotagging
+git clone git@github.com:Nosto/nosto-magento2-cmp.git src/vendor/nosto/module-nostotagging-cmp
+git clone git@github.com:Nosto/nosto-magento2-msi.git src/vendor/nosto/module-nosto-msi
+git clone git@github.com:Nosto/nosto-php-sdk.git src/vendor/nosto/php-sdk
+cp env/nosto.env src/vendor/nosto/php-sdk/src/.env
+bin/copytocontainer vendor/nosto
+bin/magento module:enable --clear-static-content Nosto_Tagging Nosto_Cmp Nosto_Msi
+bin/magento setup:upgrade
+bin/magento setup:di:compile
+bin/magento setup:static-content:deploy -f
+bin/magento cache:clean
+echo "You should be set for local development. Remember to add magento src to PHPStorm for code completion and uncomment the plugins path from the composer file"
